2c90cbfe4bb7f512c7932546abcb8d7592dfeb3d
florian-guily <floguily@gmail.com>
2023-01-31 16:05:38 +0100

    extraction parallelization (WIP)

--- etl.py
+++ etl.py
@@ -4,45 +4,88 @@ from pathlib import Path
 import pandas as pd
 from datetime import datetime as dt
 from zipfile import ZipFile
-import re
+import re, time, requests
+from multiprocessing import cpu_count
+from multiprocessing.pool import ThreadPool
 
 ext_dict = {'export': 'CSV',
             'mentions': 'CSV',
             'gkg': 'csv'}
 
-def download_zip(date, type, extension, dir_data, trans):
-    str_date = date.strftime("%Y%m%d%H%M%S")
-    if trans==True:
+dir_data = './data/raw'
 
-        url = f'http://data.gdeltproject.org/gdeltv2/{str_date}.translation.{type}.{extension}.zip'
-        filename = f'{str_date}.translation.{type}.{extension}.zip'
+def create_urls(date_range, type, trans):
+    extension = ext_dict[type]
+    if trans:
+        urls = [f'http://data.gdeltproject.org/gdeltv2/{date.strftime("%Y%m%d%H%M%S")}.translation.{type}.{extension}.zip' for date in date_range]
     else:
-        url = f'http://data.gdeltproject.org/gdeltv2/{str_date}.{type}.{extension}.zip'
-        filename = f'{str_date}.{type}.{extension}.zip'
-    filepath = f'{dir_data}/{filename}'
-    try:
-        if not os.path.exists(filepath):
-            wget.download(url, filepath)
-    except Exception as e:
-        print(f'Error for {filename}')
-        print(e)
-    return filepath, filename
+        urls = [f'http://data.gdeltproject.org/gdeltv2/{date.strftime("%Y%m%d%H%M%S")}.{type}.{extension}.zip' for date in date_range]
+    return urls
 
-def unzip(filepath, filename, dir_data):
+def create_fns(date_range, type, trans):
+    extension = ext_dict[type]
+    if trans:
+        fns = [f'{date.strftime("%Y%m%d%H%M%S")}.translation.{type}.{extension}.zip' for date in date_range]
+    else:
+        fns = [f'{date.strftime("%Y%m%d%H%M%S")}.{type}.{extension}.zip' for date in date_range]
+    return fns
+
+def download_url(args):
+    t0 = time.time()
+    url, fn = args[0], args[1]
+    try:
+        r = requests.get(url)
+        with open(fn, 'wb') as f:
+            f.write(r.content)
+        return(url, time.time() - t0)
+    except Exception as e:
+        print('Exception in download_url():', e)
+
+def download_zip(args):
+    t0 = time.time()
+    url, fn = args[0], args[1]
+    filepath = f'{dir_data}/{fn}'
+    try:
+        wget.download(url, filepath)
+    except Exception as e:
+        print(f'Error for {fn}')
+        print(e)
+    # return filepath, fn
+    return(filepath, time.time() - t0)
+
+def unzip(args):
     # loading the temp.zip and creating a zip object
+    filepath= args[0]
     with ZipFile(filepath, 'r') as zObject:
         # Extracting all the members of the zip 
         # into a specific location.
-        path = zObject.extract(member=filename.replace('.zip', ''), path=dir_data)
-    if os.path.exists(filepath):
-        os.remove(filepath)
-    os.rename(path, path.replace('CSV', 'csv'))
+        zObject.extractall(path=dir_data)
+    csv_path = filepath[:-4]
+    os.remove(filepath)
+    os.rename(csv_path, csv_path.replace('CSV', 'csv'))
 
-def extract(start, end, type, dir_data, trans):
-    date_range = pd.date_range(start, end, freq='15T').to_pydatetime()
-    for date in date_range:
-        filepath, filename = download_zip(date, type, ext_dict[type], dir_data, trans)
-        unzip(filepath, filename, dir_data)
+def download_parallel(args):
+    cpus = cpu_count()
+    pool = ThreadPool(cpus - 1)
+    results = pool.imap_unordered(download_zip, args, chunksize=10)
+    pool.close()
+    pool.join()
+    # for result in results:
+    #     print('url:', result[0], 'time (s):', result[1])
+    return results
+
+def unzip_parallel(args):
+    cpus = cpu_count()
+    pool = ThreadPool(cpus - 1)
+    results = pool.imap_unordered(unzip, args, chunksize=5)
+    pool.close()
+    pool.join()
+
+def extract(inputs):
+    for input in inputs:
+        result = download_zip(input)
+        unzip(result)
+        #print('url:', result[0], 'time (s):', result[1])
 
 def transform(dir_data, start, end, type, trans):
     path = Path(dir_data)
@@ -76,12 +119,20 @@ def main():
     start = dt.strptime(sys.argv[1], '%Y%m%d%H')
     end = dt.strptime(sys.argv[2], '%Y%m%d%H')
     type = sys.argv[3]
-    trans = sys.argv[4]
+    trans = sys.argv[4] == 'True'
 
-    dir_data = './data/raw'
+    date_range = pd.date_range(start, end, freq='15T').to_pydatetime()
+    urls = create_urls(date_range, type, trans)
+    fns = create_fns(date_range, type, trans)
+    inputs = zip(urls, fns)
 
-    extract(start, end, type, dir_data, trans)
-    transform(dir_data, start, end, type, trans)
+    t0 = time.time()
+    results = download_parallel(inputs)
+    unzip_parallel(results)
+    # extract(inputs)
+    print(f"Total time: {time.time() - t0}")
+    # extract(start, end, type, dir_data, trans)
+    # transform(dir_data, start, end, type, trans)
 
 if __name__=='__main__':
     main()
