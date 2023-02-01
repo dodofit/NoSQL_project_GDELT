44beee1733aa4aac666bcd454ec48de7152f9129
dodofit <fittondorian@gmail.com>
2023-02-01 22:50:31 +0100

    transform with headers

--- etl.py
+++ etl.py
@@ -16,19 +16,19 @@ dir_data = './data/raw'
 
 def create_urls(date_range, type, trans):
     extension = ext_dict[type]
-    if trans:
-        urls = [f'http://data.gdeltproject.org/gdeltv2/{date.strftime("%Y%m%d%H%M%S")}.translation.{type}.{extension}.zip' for date in date_range]
-    else:
-        urls = [f'http://data.gdeltproject.org/gdeltv2/{date.strftime("%Y%m%d%H%M%S")}.{type}.{extension}.zip' for date in date_range]
-    return urls
+    #if trans:
+    urls_trans = [f'http://data.gdeltproject.org/gdeltv2/{date.strftime("%Y%m%d%H%M%S")}.translation.{type}.{extension}.zip' for date in date_range]
+    #else:
+    urls = [f'http://data.gdeltproject.org/gdeltv2/{date.strftime("%Y%m%d%H%M%S")}.{type}.{extension}.zip' for date in date_range]
+    return urls,urls_trans
 
 def create_fns(date_range, type, trans):
     extension = ext_dict[type]
-    if trans:
-        fns = [f'{date.strftime("%Y%m%d%H%M%S")}.translation.{type}.{extension}.zip' for date in date_range]
-    else:
-        fns = [f'{date.strftime("%Y%m%d%H%M%S")}.{type}.{extension}.zip' for date in date_range]
-    return fns
+    #if trans:
+    fns_trans = [f'{date.strftime("%Y%m%d%H%M%S")}.translation.{type}.{extension}.zip' for date in date_range]
+    #else:
+    fns = [f'{date.strftime("%Y%m%d%H%M%S")}.{type}.{extension}.zip' for date in date_range]
+    return fns,fns_trans
 
 def download_url(args):
     t0 = time.time()
@@ -87,29 +87,73 @@ def extract(inputs):
         unzip(result)
         #print('url:', result[0], 'time (s):', result[1])
 
-def transform(dir_data, start, end, type, trans):
-    path = Path(dir_data)
-    if trans==True:
-        files = list(path.glob(f'*translation.{type}.csv'))
-    else:
-        files = list(path.glob(f'*0.{type}.csv'))
-    header_gkg = ['GKGRecordID', 'DATE', 'SourceCollectionIdentifier', 'SourceCommonName', 'DocumentIdentifier','Counts', 'V2Counts', 'Themes', 'V2Themes', 'Locations', 'V2Locations', 'Persons', 'V2Persons','Organizations', 'V2Organizations', 'V2Tone', 'Dates', 'GCAM', 'SharingImage', 'RelatedImages','SocialImageEmbeds', 'SocialVideoEmbeds', 'Quotations', 'AllNames', 'Amounts', 'TranslationInfo','Extras']
-    df = pd.concat([pd.read_csv(f,delimiter="\t",
-                                header=None,
-                                on_bad_lines=None,
-                                encoding='ISO-8859-1',
-                                names=header_gkg,
-                                dtype={'DATE':"object", 'GKGRecordID':'object'}
-                                )
-                    for f in files
-                    ])
-
-    if trans==True:
-        df.to_csv(dir_data+f'/dataset_trans_{type}_from_{start}_to_{end}.csv')
+#def sequential():
+#    for input in inputs:
+#        result = download_zip(input)
+#        unzip(result)
+#        #print('url:', result[0], 'time (s):', result[1])
+def transform(dir_data, file, start, end, type):
+    #path = Path(dir_data)
+    #files = list(path.glob(f'*translation.{type}.csv'))
+    if type=='gkg':
+        headers = ['GKGRecordID', 'DATE', 'SourceCollectionIdentifier', 'SourceCommonName', 'DocumentIdentifier',
+                   'Counts', 'V2Counts', 'Themes', 'V2Themes', 'Locations', 'V2Locations', 'Persons', 'V2Persons',
+                   'Organizations', 'V2Organizations', 'V2Tone', 'Dates', 'GCAM', 'SharingImage', 'RelatedImages',
+                   'SocialImageEmbeds', 'SocialVideoEmbeds', 'Quotations', 'AllNames', 'Amounts', 'TranslationInfo','Extras']
+        df = pd.read_csv(file, delimiter="\t",
+                                    header=None,
+                                    on_bad_lines=None,
+                                    encoding='ISO-8859-1',
+                                    names=headers,
+                                    dtype={'DATE': "object", 'GKGRecordID': 'object', 'SourceCollectionIdentifier':'object'}
+                                    )
         df['TranslationInfo'] = df['TranslationInfo'].astype(str).apply(
             lambda x: re.sub(r'(srclc:)([a-z]+)(.*)', r'\2', x))  # extracting language information
+    elif type=='mentions':
+        headers = ['GlobalEventID', 'EventTimeDate', 'MentionTimeDate', 'MentionType', 'MentionSourceName',
+                   'MentionIdentifier', 'SentenceID', 'Actor1CharOffset', 'Actor2CharOffset', 'ActionCharOffset',
+                   'InRawText', 'Confidence', 'MentionDocLen', 'MentionDocTone', 'MentionDocTranslationInfo','Extras']
+        df = pd.read_csv(file,delimiter="\t",
+                                    header=None,
+                                    on_bad_lines=None,
+                                    encoding='ISO-8859-1',
+                                    names=headers,
+                                    dtype={'GlobalEventID':'object', 'EventTimeDate':'object', 'MentionTimeDate':'object',
+       'MentionType':'object'}
+                         )
+
+
+        df['MentionDocTranslationInfo'] = df['MentionDocTranslationInfo'].astype(str).apply(
+            lambda x: re.sub(r'(srclc:)([a-z]+)(.*)', r'\2', x))  # extracting language information
     else:
-        df.to_csv(dir_data + f'/dataset_{type}_from_{start}_to_{end}.csv')#
+        headers= ['GlobalEventID','Day',	'MonthYear',	'Year',	'FractionDate',	'Actor1Code',
+                  'Actor1Name',	'Actor1CountryCode',	'Actor1KnownGroupCode',	'Actor1EthnicCode',
+                  'Actor1Religion1Code',	'Actor1Religion2Code',	'Actor1Type1Code',	'Actor1Type2Code',
+                  'Actor1Type3Code',	'Actor2Code',	'Actor2Name',	'Actor2CountryCode',
+                  'Actor2KnownGroupCode',	'Actor2EthnicCode',	'Actor2Religion1Code',	'Actor2Religion2Code',
+                  'Actor2Type1Code',	'Actor2Type2Code',	'Actor2Type3Code',	'IsRootEvent',	'EventCode',
+                  'EventBaseCode',	'EventRootCode',	'QuadClass',	'GoldsteinScale',	'NumMentions',
+                  'NumSources',	'NumArticles',	'AvgTone',	'Actor1Geo_Type',	'Actor1Geo_FullName',
+                  'Actor1Geo_CountryCode',	'Actor1Geo_ADM1Code',	'Actor1Geo_ADM2Code',	'Actor1Geo_Lat',
+                  'Actor1Geo_Long',	'Actor1Geo_FeatureID',	'Actor2Geo_Type',	'Actor2Geo_FullName',
+                  'Actor2Geo_CountryCode',	'Actor2Geo_ADM1Code',	'Actor2Geo_ADM2Code',	'Actor2Geo_Lat',
+                  'Actor2Geo_Long',	'Actor2Geo_FeatureID',	'ActionGeo_Type',	'ActionGeo_FullName',
+                  'ActionGeo_CountryCode',	'ActionGeo_ADM1Code',	'ActionGeo_ADM2Code',	'ActionGeo_Lat',
+                  'ActionGeo_Long',	'ActionGeo_FeatureID',	'DATEADDED',	'SOURCEURL']
+        df = pd.read_csv(file,delimiter="\t",
+                                    header=None,
+                                    on_bad_lines=None,
+                                    encoding='ISO-8859-1',
+                                    names=headers,
+                                    dtype={'GlobalEventID':'object', 'Day':'object', 'MonthYear':'object', 'Year':'object','FractionDate': 'object','IsRootEvent':'object','EventCode':'object', 'EventBaseCode':'object', 'EventRootCode':'object', 'QuadClass':'object','Actor1Geo_Type':'object','Actor2Geo_Type':'object','ActionGeo_Type':'object'}
+                                    )
+    dir_dest = dir_data+'/headers_'+str(file)[9:-3]+'pkl'
+    print(dir_dest)
+    df.to_pickle(dir_dest)
+
+    return None
+
+
 
 
 def load():
@@ -120,10 +164,11 @@ def main():
     end = dt.strptime(sys.argv[2], '%Y%m%d%H')
     type = sys.argv[3]
     trans = sys.argv[4] == 'True'
-
     date_range = pd.date_range(start, end, freq='15T').to_pydatetime()
-    urls = create_urls(date_range, type, trans)
-    fns = create_fns(date_range, type, trans)
+    urls, urls_trans = create_urls(date_range, type, trans)
+    fns, fns_trans = create_fns(date_range, type, trans)
+    urls = urls+urls_trans
+    fns=fns+ fns_trans
     inputs = zip(urls, fns)
 
     t0 = time.time()
@@ -132,7 +177,11 @@ def main():
     # extract(inputs)
     print(f"Total time: {time.time() - t0}")
     # extract(start, end, type, dir_data, trans)
-    # transform(dir_data, start, end, type, trans)
 
+    path = Path(dir_data)
+    files = list(path.glob(f'*.{type}.csv'))
+    for file in files:
+        transform(dir_data,file, start, end, type)
+        os.remove(file)
 if __name__=='__main__':
     main()
